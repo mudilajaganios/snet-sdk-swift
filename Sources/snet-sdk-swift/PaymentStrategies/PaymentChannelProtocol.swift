d1f521a0acba567dcf4b762b4d44121d4a1c6502
Jagan Kumar Mudila <69246481+mudilajaganios@users.noreply.github.com>
2021-04-23 13:21:20 +0400

    Added a protocol for the Payment Strategy

---
+++ Sources/snet-sdk-swift/PaymentStrategies/PaymentChannelProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  PaymentChannelProtocol.swift
+//  snet-sdk-swift
+//
+//  Created by Jagan Kumar Mudila on 23/04/2021.
+//
+
+import Foundation
+
+protocol PaymentChannelProtocol: class {
+    func getPaymentMetadata()
+}
